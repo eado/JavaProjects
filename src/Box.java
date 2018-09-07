1b05e552afd9a2d7fce0c9a61bfef4956fb6d163
Omar Elamri <ea_do@icloud.com>
2018-09-07 08:26:55 -0500

    initial

---
+++ src/Box.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Box {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("name\tdate");
+        int i = 0;
+        while (i < 3) {
+            i++;
+            int plus = 0;
+            while (plus < 3) {
+                plus++;
+                int str = 0;
+                while (str < 20) {
+                    str++;
+                    System.out.print("+");
+                }
+                System.out.print("\n");
+            }
+
+            int a = 0;
+            while (a < 2) {
+                a++;
+                int str = 0;
+                while (str < 20) {
+                    str++;
+                    System.out.print("A");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+}
